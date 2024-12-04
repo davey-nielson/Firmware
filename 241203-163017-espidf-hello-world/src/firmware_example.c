ef75134aa6760a307c9d462539891c474db69bb1
DavidNielsonAmano <153562421+DavidNielsonAmano@users.noreply.github.com>
2024-12-03 18:13:21 -0700

    Add files via upload

---
+++ 241203-163017-espidf-hello-world/src/firmware_example.c
@@ -0,0 +1,35 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+
+// Libraries
+
+// Wi-Fi configuration
+#define WIFI_SSID "AmanoIoT"
+#define WIFI_PASSWORD "cleanwater!"
+
+// API Endpoints
+#define POST_URL "https://amano-api-945679465613.us-west3.run.app/v1/deviceInterface/AMANO_D4D4DA724E00/firmware"
+
+//Connect to WiFi
+
+//Send data to POST endpoint
+
+//Parse Data from response
+
+//Print out binaryURL string
+
+// Main Function
+void app_main(void)
+{
+
+}
